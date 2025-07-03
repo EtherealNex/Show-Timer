bd0bb53b74b1371397082cb63f8a399d74a6191d
= <nex.handley@icloud.com>
2025-07-03 10:41:18 +0100

    refactored loss of themeing + some comments

--- src/core/models.py
+++ src/core/models.py
@@ -4,10 +4,12 @@ from dataclasses import dataclass, field
 import uuid
 
 def generate_uuid():
+    """Generates a UUID4, returing its value to be used externally for spesific ID's."""
     return uuid.uuid4()
 
 @dataclass
 class Call:
+    """Creates a UUUID, Needs a label, and a duration"""
     id: str = field(default_factory=lambda: str(generate_uuid()))
     label: str = "Untitled Call" # The name for the Cal
     duration: float = 300 # Length of call in seconds, default 5m (300s)
